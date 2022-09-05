11dd1a2a0a11958ddb083bb4175803ee7799e5af
Fahad Hossain <fhfahad@amazon.com>
2022-09-05 14:31:45 +0200

    feat: add neovim and hub-cli

--- fish/.config/fish/env.fish
+++ fish/.config/fish/env.fish
@@ -1,8 +1,1 @@
-set -gx HOMEBREW_PREFIX "/opt/homebrew";
-set -gx HOMEBREW_CELLAR "/opt/homebrew/Cellar";
-set -gx HOMEBREW_REPOSITORY "/opt/homebrew";
-set -q PATH; or set PATH ''; set -gx PATH "/opt/homebrew/bin" "/opt/homebrew/sbin" $PATH;
-set -q MANPATH; or set MANPATH ''; set -gx MANPATH "/opt/homebrew/share/man" $MANPATH;
-set -q INFOPATH; or set INFOPATH ''; set -gx INFOPATH "/opt/homebrew/share/info" $INFOPATH;
-
-set -gx PATH "/opt/homebrew/opt/python@3.10/libexec/bin" $PATH;
+# env stuff goes here
--- install.sh
+++ install.sh
@@ -3,18 +3,19 @@
 SCRIPT="$(pwd)"
 
 if [ ! -f /etc/yum.repos.d/shells:fish:release:3.repo ]; then
-  cd /etc/yum.repos.d/
-  sudo wget https://download.opensuse.org/repositories/shells:fish:release:4/CentOS_7/shells:fish:release:3.repo
+    cd /etc/yum.repos.d/
+    sudo wget https://download.opensuse.org/repositories/shells:fish:release:4/CentOS_7/shells:fish:release:3.repo
 fi
-sudo yum install fish git fortune-mod rsync
 
+sudo yum install fish git fortune-mod rsync curl tar -y
+sudo yum --enablerepo epel install neovim python3-neovim -y
 
 if [ ! -d $HOME/n ]; then
-  curl -L https://bit.ly/n-install | bash -s -- -y lts
+    curl -L https://bit.ly/n-install | bash -s -- -y lts
 fi
 
 if [ -z "$(which starship)" ]; then
-  curl -sS https://starship.rs/install.sh | sh
+    curl -sS https://starship.rs/install.sh | sh
 fi
 
 read -p "Replace fish config [y/N]? " -n 1 -r
@@ -55,23 +56,30 @@ EOF
 
 fi
 
-if [ ! -d $HOME/.config/omf ]; then
-  curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
-fi
+rsync $SCRIPT/fish/ $HOME/ -avhP
 
-cat <<'EOF' > $HOME/.config/omf/bundle
-package bang-bang
-package bass
-package sudope
-package z
-theme default
+if [ ! -d $HOME/.config/omf ]; then
+    curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
+
+    cat <<-EOF > $HOME/.config/omf/bundle
+    package bang-bang
+    package bass
+    package sudope
+    package z
+    theme default
 EOF
 
-touch $HOME/.run-omf-install
+    touch $HOME/.run-omf-install
+fi
+
 
 $HOME/n/bin/npm i -g tldr eslint prettier
 
-rsync $SCRIPT/fish/ $HOME/ -avhP
+if [ -z "$(which hub)" ]; then
+    mkdir -p /tmp/hub
+    cd /tmp/hub
+    curl -fsSL "https://github.com/github/hub/releases/download/v2.14.2/hub-linux-amd64-2.14.2.tgz" | tar -xz --strip-components=1
+    sudo ./install
+fi
 
 chsh -s $(which fish)
-
